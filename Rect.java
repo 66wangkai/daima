2faebb18da801f2adab89696fb48cbf6a39e490d
Wangkai <483348772@qq.com>
2020-10-04 22:38:26 +0800

    矩形的面积

---
+++ Rect.java
@@ -0,0 +1,17 @@
+public class Rect {
+    double width;
+    double height;
+    double getArea(){
+        return width * height;
+    }
+}
+class Example1_2 {
+    public static void main (string args[]){
+    Rect rectangle;
+    Rectangle = new Rect();
+    Rectangle.width = 1.819;
+    Rectangle.height = 1.5;
+    double area = rectangle.getArea();
+    System.out. println("矩形的面积:"+ area)
+    }
+}
